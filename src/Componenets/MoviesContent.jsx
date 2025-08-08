792f6cc23dec4c2baea537f779960ea21f4d0e68
Tommy0412 <37461047+Tommy0412@users.noreply.github.com>
2025-08-08 15:49:57 +0200

    Update MoviesContent.jsx

--- src/Componenets/MoviesContent.jsx
+++ src/Componenets/MoviesContent.jsx
@@ -8,30 +8,41 @@ function MoviesContent() {
     const [data, setData] = useState([]);
     const sort = useSelector(selectSort);
     const page = useSelector(selectPage);
-	const key = import.meta.env.VITE_TMDB_API_KEY;
+    const key = import.meta.env.VITE_TMDB_API_KEY;
+	const siteName = import.meta.env.VITE_SITE_NAME;
 
     useEffect(() => {
-        if (sort === 'popular') {
-            fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${key}&include_adult=false&include_null_first_air_dates=false&language=en-US&page=${page}&sort_by=vote_average.desc&vote_count.gte=500&without_genres=16`)
-            .then((res) => res.json())
-            .then((data) => setData(data.results));
-            document.title = 'embedsito - Popular Movies & TV Shows watch online';
-        } else if (sort === 'top_rated') {
-            fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${key}&include_adult=false&include_null_first_air_dates=false&language=en-US&page=${page}&sort_by=vote_count.desc&without_genres=16`)
-            .then((res) => res.json())
-            .then((data) => setData(data.results));
-            document.title = 'embedsito - Top Rated Movies & TV Shows watch online';
+        let url = "";
+        let title = "";
+
+        if (sort === "popular") {
+            url = `https://api.themoviedb.org/3/discover/movie?api_key=${key}&include_adult=false&include_null_first_air_dates=false&language=en-US&page=${page}&sort_by=vote_average.desc&vote_count.gte=500&without_genres=16`;
+			title = `${siteName} - Popular Movies watch online`;
+        } else if (sort === "top_rated") {
+            url = `https://api.themoviedb.org/3/discover/movie?api_key=${key}&include_adult=false&include_null_first_air_dates=false&language=en-US&page=${page}&sort_by=vote_count.desc&without_genres=16`;
+			title = `${siteName} - Top Rated Movies watch online`;
+        } else if (sort === "now_playing") {
+            url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${key}&include_adult=false&include_null_first_air_dates=false&language=en-US&page=${page}&without_genres=16`;
+			title = `${siteName} - Now Playing Movies online`;
         }
-        window.scrollTo(0,0);
-    }, [sort, page]);
+
+        if (url) {
+            fetch(url)
+                .then((res) => res.json())
+                .then((data) => setData(data.results || []));
+            document.title = title;
+        }
+
+        window.scrollTo(0, 0);
+    }, [sort, page, key]);
 
     return (
         <div className="my-4 grid grid-cols-7 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
             {data.map((movie, index) => (
-                <ShowCard key={index} movie={movie} index={index} value={0} type={'movie'} />
+                <ShowCard key={index} movie={movie} index={index} value={0} type={"movie"} />
             ))}
         </div>
-    )
+    );
 }
 
 export default MoviesContent;
