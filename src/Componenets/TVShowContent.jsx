093c7128579fe24582ce9ad4145fd0ed57c4a8f9
Tommy0412 <37461047+Tommy0412@users.noreply.github.com>
2025-08-08 15:51:20 +0200

    Update TVShowContent.jsx

--- src/Componenets/TVShowContent.jsx
+++ src/Componenets/TVShowContent.jsx
@@ -8,22 +8,33 @@ function TVShowSlider() {
     const [data, setData] = useState([]);
     const sort = useSelector(selectSort);
     const page = useSelector(selectPage);
-	const key = import.meta.env.VITE_TMDB_API_KEY;
+    const key = import.meta.env.VITE_TMDB_API_KEY;
+	const siteName = import.meta.env.VITE_SITE_NAME;
 
     useEffect(() => {
+        let url = "";
+        let title = "";
+
         if (sort === 'popular') {
-            fetch(`https://api.themoviedb.org/3/discover/tv?api_key=${key}&include_adult=false&include_null_first_air_dates=false&language=en-US&page=${page}&sort_by=vote_average.desc&vote_count.gte=500&without_genres=16`)
-            .then((res) => res.json())
-            .then((data) => setData(data.results));
-            document.title = 'embedsito - Popular Movies Watch Online';
+            url = `https://api.themoviedb.org/3/discover/tv?api_key=${key}&include_adult=false&include_null_first_air_dates=false&language=en-US&page=${page}&sort_by=vote_average.desc&vote_count.gte=500&without_genres=16`;
+			title = `${siteName} - Popular TV Shows Watch Online`;
         } else if (sort === 'top_rated') {
-            fetch(`https://api.themoviedb.org/3/discover/tv?api_key=${key}&include_adult=false&include_null_first_air_dates=false&language=en-US&page=${page}&sort_by=vote_count.desc&without_genres=16`)
-            .then((res) => res.json())
-            .then((data) => setData(data.results));
-            document.title = 'embedsito - Top Rated TV Shows Watch Online';
+            url = `https://api.themoviedb.org/3/discover/tv?api_key=${key}&include_adult=false&include_null_first_air_dates=false&language=en-US&page=${page}&sort_by=vote_count.desc&without_genres=16`;
+			title = `${siteName} - Top Rated TV Shows Watch Online`;
+        } else if (sort === 'now_playing') {
+            url = `https://api.themoviedb.org/3/tv/on_the_air?api_key=${key}&include_adult=false&include_null_first_air_dates=false&language=en-US&page=${page}&without_genres=16`;
+			title = `${siteName} - Now Playing TV Shows Watch Online`;
         }
-        window.scrollTo(0,0)
-    }, [sort, page]);
+
+        if (url) {
+            fetch(url)
+                .then((res) => res.json())
+                .then((data) => setData(data.results || []));
+            document.title = title;
+        }
+
+        window.scrollTo(0,0);
+    }, [sort, page, key]);
 
     return (
         <div className="my-4 grid grid-cols-7 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
